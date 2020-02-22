9968f4c4274a0ebd5416de8ccb1c7f120106c762
Vladimir Petrov <vppetrov@mm-sol.com>
2020-02-22 21:34:48 +0200

    gen2xcd: toc: Fixed dataname

--- gen2xcd.c
+++ gen2xcd.c
@@ -150,7 +150,7 @@ generate_image (const char *base_name)
 		}
 	      if (CD_OK == ret)
 		{
-		  ret = write_silence (trk_i, &pos, cdimg, toc, cue, cdimg_name);
+		  ret = write_silence (trk_i, &pos, cdimg, toc, cue, base_name);
 		}
 	    }
 	  fclose (cdimg);
